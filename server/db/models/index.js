46e889dc18f389d940383c58d94417073b78a877
Bianca Sherika <bianca_sherika@hotmail.com>
2021-03-18 20:04:32 -0300

    bug

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -7,7 +7,6 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-const port= process.env.Port || 3000
 
 let sequelize;
 if (config.use_env_variable) {
