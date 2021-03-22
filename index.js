9b7fca2cbcabef68da3557770cc5a01d00dac798
Bianca Sherika <bianca_sherika@hotmail.com>
2021-03-22 16:33:40 -0300

    atualizado gitignore e deploy heroku

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const routes = require('./server/routes/index')
+const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors());
 
 app.use('/', routes);
 
 app.get('*', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!')
 })
 
 app.listen(port, () => {
