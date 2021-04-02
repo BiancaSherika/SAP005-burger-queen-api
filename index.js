ff9d8bcd6ff2e6073b3b15cd8039d363fba6e2d9
Bianca Sherika <bianca_sherika@hotmail.com>
2021-04-02 19:27:42 -0300

    fix bug deploy dotenv

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const routes = require('./server/routes/index')
 const cors = require("cors")
+const dotenv = require('dotenv')
 const port = process.env.PORT || 3000
-
 const app = express()
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
 
 app.use(cors());
 app.use(express.json());
